731035d41e894b8ebf3f91592c6fe64869c46747
floresernesto95 <floresernesto1995@gmail.com>
2025-01-23 04:36:36 -0300

    upt settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -110,6 +110,17 @@ DATABASES = {
     )
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
